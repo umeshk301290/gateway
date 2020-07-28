1399d0d3d29ff0e1961160f47ca1942922e1a021
umeshk301290 <55276171+umeshk301290@users.noreply.github.com>
2020-07-28 20:15:56 +0530

    Add files via upload

---
+++ src/main/java/com/example/gateway/GatewayApplication.java
@@ -0,0 +1,24 @@
+package com.example.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+	
+	 @Bean
+	    public BCryptPasswordEncoder passwordEncoder() {
+	        return new BCryptPasswordEncoder(); // For encrypting user password
+	    }
+
+}
